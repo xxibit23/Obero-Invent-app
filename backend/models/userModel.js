07983e771053e45661d588eda754fb73b675e9e8
xxibit23 <siandsi6677@gmail.com>
2024-05-02 21:50:01 -0800

    new commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// user model containing validating infos
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        ]
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
