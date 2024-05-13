11b85c7e01ad4fccdc1b08e3084ba48883514db4
xxibit23 <siandsi6677@gmail.com>
2024-05-13 00:48:07 -0800

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home/Home';
+
 
 function App() {
   return (
-    <BrowserRouter>   {/* must surrounds all routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h2>Hello World</h2>
+    </div>
   );
 }
 
